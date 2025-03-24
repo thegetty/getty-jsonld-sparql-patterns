648188ee01c7fc9ac185da05716f59d81cd47b6d
bosteen@getty.edu <bosteen@getty.edu>
2025-03-24 13:53:36 -0700

    Adding two new counts to la_counts, version v1.0.2

--- src/gettysparqlpatterns/patterns/la_counts.py
+++ src/gettysparqlpatterns/patterns/la_counts.py
@@ -55,4 +55,40 @@ SELECT (count(distinct ?inf) as ?count) WHERE {
 } LIMIT 1""",
         "stype": "count",
     },
+    {
+        "name": "count_hmos_with_nonexistant_visitems",
+        "description": "",
+        "sparql_pattern": """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/> 
+SELECT (count (distinct ?images) as ?count) WHERE {  
+  ?hmo crm:P65_shows_visual_item ?images .  
+  FILTER NOT EXISTS {  
+    SERVICE <$VISITEM_SERVICE> {  
+      ?images rdf:type ?visimg .  
+    }
+  }
+} LIMIT 1""",
+        "stype": "count",
+        "default_values": {
+            "VISITEM_SERVICE": "https://data.jpcarchive.org/media/sparql"
+        },
+    },
+    {
+        "name": "count_hmos_with_existing_visitems",
+        "description": "",
+        "sparql_pattern": """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/> 
+SELECT (count (distinct ?images) as ?count) WHERE {  
+  ?hmo crm:P65_shows_visual_item ?images .  
+  FILTER EXISTS {  
+    SERVICE <$VISITEM_SERVICE> {  
+      ?images rdf:type ?visimg .  
+    }
+  }
+} LIMIT 1""",
+        "stype": "count",
+        "default_values": {
+            "VISITEM_SERVICE": "https://data.jpcarchive.org/media/sparql"
+        },
+    },
 ]
